895b7f432d6eaa434dc47e8a73779d9a293ea545
Arihant-3 <aburnwal26@gmail.com>
2025-06-12 10:51:02 +0530

    Model Training Done

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,44 @@ import numpy as np
 import pandas as pd 
 import dill
 
+from sklearn.metrics import r2_score
+
 from src.exception import CustomException
+
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    """
+    Evaluates the performance of different models.
+    
+    Parameters:
+    X_train (np.ndarray): Training features.
+    y_train (np.ndarray): Training labels.
+    X_test (np.ndarray): Testing features.
+    y_test (np.ndarray): Testing labels.
+    models (dict): Dictionary of models to evaluate.
+    
+    Returns:
+    dict: A dictionary containing model names and their respective scores.
+    """
+    try:
+        model_report = {}
+        
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            
+            model.fit(X_train, y_train)
+            
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+                
+            model_report[list(models.keys())[i]] = test_model_score
+            
+        return model_report
+    
+    except Exception as e:
+        raise CustomException(e, sys)
 
 def save_object(file_path, obj):
     """
